3de12bb0c4af406207bd909f74e5381939b2cfe3
usmantariqdev <muhammad.usman@septemsystems.com>
2023-04-20 14:06:33 +0500

    Update Data

--- app/Http/Controllers/SwiftController.php
+++ app/Http/Controllers/SwiftController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Psr7\Stream;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use OpenStack\ObjectStore\v1\Models\StorageObject;
 use OpenStack\OpenStack;
@@ -20,6 +22,7 @@ class SwiftController extends Controller
         $params = [
             'authUrl' => env('OS_AUTH_URL'),
             'region'  => 'RegionOne',
+            'tenantId' => env('OS_TENANT'),
             'user'    =>
                 array(
                     'name' => env('OS_USERNAME') ,
@@ -34,75 +37,26 @@ class SwiftController extends Controller
         $this->openStack = new OpenStack($params);
     }
 
-    public function objectStorageConnection() {
-        dd($this->downloadObject('100free', 'wallhaven-188424.jpg'));
-        $service = $this->openStack->objectStoreV1();
-        $container = $service->createContainer([
-            'name' => 'DevContainer'
-        ]);
-        dd($container->getMetadata());
-    }
-    public function createDirectory() {
-        $openstack = new OpenStack([
-            'authUrl' => 'https://authenticate.ain.net/',
-            'region' => 'RegionOne',
-            'user' => [
-                'id' => 'v2.0',
-                'password' => '95ncjPdj4Mv#jd?dja(JdVc84A',
-                'domain' => [
-                    'name' => 'YOUR_DOMAIN_NAME'
-                ]
-            ],
-//            'scope' => [
-//                'project' => [
-//                    'id' => 'YOUR_PROJECT_ID'
-//                ]
-//            ]
-        ]);
-        $container = $openstack->objectStoreV1()->getContainer('justfree');
-        $directoryName = 'dev-septem/';
-        $container->createObject(['name' => $directoryName]);
-        $object = $container->getObject($directoryName);
-        $contentType = $object->getContentType();
+    public function objectStorageConnection(): array|string
+    {
+        $containerName = 'DevContainer';
+        dd($this->downloadObject($containerName, 'Test') );
+//        $containerName = '100free';
+        $objectStore = $this->openStack->objectStoreV1();
+        $metadata = [
+            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => 'GET, PUT, POST, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization',
+            'read' => '.r:*',
+            'X-Container-Read' => '.r:*'
+        ];
 
-        if ($contentType === 'application/directory') {
-            // directory exists
-        } else {
-            // directory does not exist
+        try {
+            return $this->fileVerification($containerName);
+            dd($this->mergeMetaData($containerName));
+        } catch (Exception $e) {
+            return $e->getMessage();
         }
-    }
-    public function uploadFile(): string {
-        // Initialize the OpenStack client
-        $openstack = new OpenStack([
-            'authUrl' => 'https://your-auth-url/v3',
-            'region' => 'your-region',
-            'user' => [
-                'id' => 'your-user-id',
-                'password' => 'your-password',
-                'domain' => [
-                    'id' => 'your-domain-id',
-                ],
-            ],
-            'scope' => [
-                'project' => [
-                    'id' => 'your-project-id',
-                ],
-            ],
-        ]);
-
-        // Create a new container in Swift
-        $container = $openstack->objectStoreV1()->createContainer([
-            'name' => 'your-container-name',
-        ]);
-
-        // Create a new object in the container
-        $object = $container->createObject([
-            'name' => 'your-file-name',
-            'content' => fopen('your-file-path', 'r'),
-        ]);
-
-        // Return a success message
-        return 'File uploaded successfully.';
     }
 
     public function createContainer($containerName): Container|string
@@ -120,6 +74,7 @@ class SwiftController extends Controller
     public function getSingleContainer($containerName): Container|string {
         $openStackService = $this->openStack->objectStoreV1();
         try {
+//            dd($openStackService->getContainer($containerName)->getMetadata());
             return $openStackService->getContainer($containerName);
         } catch(Exception $e) {
             return $e->getMessage();
@@ -140,24 +95,42 @@ class SwiftController extends Controller
         }
     }
 
-    public function createObject($containerName, $objectName, $content = null): StorageObject|string
+    public function createObject($containerName, $objectName, $contentType, $content = null): StorageObject|string
     {
         $openStackService = $this->openStack->objectStoreV1();
         try {
+            $stream = new Stream(fopen(storage_path('app\public\server/' . $objectName . '.' . $contentType), 'r'));
             $options = [
-                'name'    => $objectName,
-                'content' => $content
+                'name'   => $objectName,
+                'stream' => $stream,
             ];
-            return $openStackService->getContainer($containerName)->createObject($options);
+            return $openStackService
+                ->getContainer($containerName)
+                ->createObject($options);
         } catch (Exception $e) {
             return $e->getMessage();
         }
     }
     public function getObject($containerName, $objectName): StorageObject|string
     {
+        set_time_limit(0);
         $openStackService = $this->openStack->objectStoreV1();
         try {
-            return $openStackService->getContainer($containerName)->getObject($objectName);
+            $container = $openStackService->getContainer($containerName);
+//            $metadata = [
+//                'Access-Control-Allow-Origin' => '*',
+//                'Access-Control-Allow-Methods' => 'GET, PUT, POST, DELETE, OPTIONS',
+//                'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization',
+//            ];
+//            $openStackService->getContainer($containerName)->getObject($objectName)->mergeMetadata($metadata);
+            $object = $openStackService->getContainer($containerName)->getObject($objectName);
+//            dd($object->getPublicUri());
+//            dd($openStackService->getContainer($containerName)->getMetadata());
+            $file = $openStackService->getContainer($containerName)->getObject($objectName)->download();
+//            $response = $object->download(['saveAs' => storage_path('/app/public/server/file.mp4')]);
+//            dd($file);
+            dd(Storage::put('/public/server/video.mp4', $object->download()->getContents()));
+            return 'success';
         } catch (Exception $e) {
             return $e->getMessage();
         }
@@ -170,7 +143,7 @@ class SwiftController extends Controller
             $containerInfo = array();
             $container = $openStackService->getContainer($containerName);
             foreach ($container->listObjects() as $object) {
-                dd($object);
+                $containerInfo[] = $object;
             }
             return $containerInfo;
         } catch (Exception $e){
@@ -180,15 +153,131 @@ class SwiftController extends Controller
 
     public function downloadObject($containerName, $objectName): StorageObject|string|array
     {
+        set_time_limit(0);
+        ini_set('memory_limit', '100M');
         $openStackService = $this->openStack->objectStoreV1();
         try {
+//            dd($openStackService->getContainer($containerName)->getObject($objectName)
+//                ->download());
             $publicUri = $openStackService->getContainer($containerName)
-                ->getObject($objectName)->getPublicUri();
+                ->getObject($objectName)->download();
+            dd($publicUri->getContents());
+            dd(File::copy($publicUri->getContents(), public_path($objectName)));
+            return File::copy($publicUri, public_path($objectName));
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
 
-//            Storage::copy( public_path(), storage_path() );
-            File::copy($publicUri, public_path($objectName));
-            return $openStackService->getContainer($containerName)
-                ->getObject($objectName)->getPublicUri();
+    public function getAllFilesFromLocal(): array
+    {
+        return Storage::allFiles('public/server/');
+    }
+
+    public function getAllFilesFromServer($containerName): array|string
+    {
+        $openStackService = $this->openStack->objectStoreV1();
+        try{
+            $objectInfo = array();
+            $container = $openStackService->getContainer($containerName);
+            $i = 0;
+            foreach ($container->listObjects() as $object) {
+                $fileType = explode('/', $object->contentType);
+                $objectInfo[$i]['name'] = $object->name;
+                $objectInfo[$i]['nameWithType'] = $object->name . '.' . $fileType[1];
+                $objectInfo[$i]['hash'] = $object->hash;
+                $objectInfo[$i]['contentType'] = $object->contentType;
+                $objectInfo[$i]['contentLength'] = $object->contentLength;
+                $objectInfo[$i]['lastModified'] = $object->lastModified;
+                $i++;
+            }
+            return $objectInfo;
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function fileVerification($containerName): string|array
+    {
+        try {
+            $localFiles = $this->getAllFilesFromLocal();
+            $serverFiles = $this->getAllFilesFromServer($containerName);
+            $serverFileName = array();
+            $localFileName = array();
+            foreach ($serverFiles as $file) {
+                $serverFileName[] = strtolower(trim($file['nameWithType']));
+            }
+            $i = 0;
+            foreach ($localFiles as $file) {
+                $explodeFileName = explode('/', $file);
+                $localFileName[$i] = strtolower(trim($explodeFileName[2]));
+                $i++;
+            }
+
+            $localObjects = array_diff($localFileName, $serverFileName);
+            $serverObjects = array_diff($serverFileName, $localFileName);
+            $differenceArray = array_merge($localObjects, $serverObjects);
+
+            $response = array();
+            foreach ($differenceArray as $objName) {
+                $explodedObjectName = explode('.', $objName);
+                if(!in_array($objName, $serverFileName)) {
+                    $response[] = $this->createObject($containerName, $explodedObjectName[0], $explodedObjectName[1]);
+                }
+                if(!in_array($objName, $localFileName)) {
+                    $response[] = $this->downloadObject($containerName, $explodedObjectName[0]);
+                }
+            }
+            return $response;
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function deleteObject($containerName, $objectName): ?string
+    {
+        $openStackService = $this->openStack->objectStoreV1();
+        try {
+            $openStackService->getContainer($containerName)
+                ->getObject($objectName)
+                ->delete();
+            return 'Deleted';
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function mergeContainerMetaData($containerName): string
+    {
+        $openStackService = $this->openStack->objectStoreV1();
+        try {
+            $metadata = [
+                'Access-Control-Allow-Origin' => '*',
+                'Access-Control-Allow-Methods' => 'GET, PUT, POST, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization',
+                'read' => '.r:*'
+            ];
+            $openStackService->getContainer($containerName)
+                ->mergeMetadata($metadata);
+            return 'Updated';
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function mergeObjectMetaData($containerName, $objectName): string
+    {
+        $openStackService = $this->openStack->objectStoreV1();
+        try {
+            $metadata = [
+                'Access-Control-Allow-Origin' => '*',
+                'Access-Control-Allow-Methods' => 'GET, PUT, POST, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization',
+                'read' => '.r:*'
+            ];
+            $openStackService->getContainer($containerName)->getObject($objectName)
+                ->mergeMetadata($metadata);
+            return 'Updated';
         } catch (Exception $e) {
             return $e->getMessage();
         }
